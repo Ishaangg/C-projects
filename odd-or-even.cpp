26f7dde4f72ebfeedb805ddb44a5fc7e15051090
Ishaangg <ishaangg12@gmail.com>
2021-09-11 16:20:38 +0530

    C++ projects

---
+++ odd-or-even.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num;
+
+    cout<<"Enter a number: ";
+    cin>>num;
+
+    if(num % 2 == 0){
+        cout<<"The number is even\n";
+
+    }
+    else{
+        cout<<"The number is odd\n";
+    }
+    return 0;
+
+}
+
